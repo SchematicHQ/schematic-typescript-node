657aa78b24c91615a4cf9e11ad470c00aa8ebd01
Ben Papillon <benpapillon@gmail.com>
2023-08-07 15:12:56 -0400

    Add event capture to Typescript lib

--- main.ts
+++ main.ts
@@ -1,12 +1,14 @@
 import fetch from "node-fetch";
 
 import * as api from "./src/index";
+import { EventCapture } from "./src/events";
 
 const fetchApi: api.FetchAPI = fetch as unknown as api.FetchAPI;
 
 export interface Schematic {
   Companies: api.CompaniesApi;
   Entitlements: api.EntitlementsApi;
+  EventCapture: EventCapture;
   Events: api.EventsApi;
   Features: api.FeaturesApi;
 }
@@ -34,6 +36,7 @@ export function init(apiKey: string, opts?: SchematicOptions): Schematic {
   return {
     Companies: new api.CompaniesApi(config),
     Entitlements: new api.EntitlementsApi(config),
+    EventCapture: new EventCapture(config),
     Events: new api.EventsApi(config),
     Features: new api.FeaturesApi(config),
   };
---
+++ src/events.ts
@@ -0,0 +1,78 @@
+import zlib from "zlib";
+import querystring from "querystring";
+import { v4 as uuidv4 } from "uuid";
+import fetch from "node-fetch";
+
+import { Configuration } from "./index";
+
+type EventType = "identify" | "track";
+
+type EventBodyCompany = {
+  keys: Record<string, string>;
+  name?: string;
+  traits: Record<string, any>;
+};
+
+type EventBodyIdentify = {
+  company?: EventBodyCompany;
+  keys: Record<string, string>;
+  traits: Record<string, any>;
+};
+
+type EventBodyTrack = {
+  event: string;
+  traits: Record<string, any>;
+  company?: Record<string, string>;
+  user?: Record<string, string>;
+};
+
+type EventBody = EventBodyIdentify | EventBodyTrack;
+
+export class EventCapture {
+  private apiKey: string;
+  private basePath: string;
+
+  constructor(config: Configuration) {
+    const apiKey = config.apiKey && config.apiKey("X-Schematic-Api-Key");
+    if (!apiKey) throw new Error("Missing API key");
+
+    this.apiKey = apiKey;
+    this.basePath = config.basePath;
+  }
+
+  private async sendEvent(
+    eventType: EventType,
+    eventBody: EventBody,
+  ): Promise<void> {
+    const event = {
+      api_key: this.apiKey,
+      body: eventBody,
+      sent_at: new Date().toISOString(),
+      tracker_event_id: uuidv4(),
+      tracker_user_id: uuidv4(),
+      type: eventType,
+    };
+    const payload = JSON.stringify(event);
+
+    const compressedData = await new Promise<Buffer>((resolve, reject) => {
+      zlib.deflate(payload, (err, result) => {
+        if (err) reject(err);
+        else resolve(result);
+      });
+    });
+
+    const base64EncodedData = compressedData.toString("base64");
+    const escapedData = querystring.escape(base64EncodedData);
+    const requestUrl = `${this.basePath}/e?p=${escapedData}`;
+
+    await fetch(requestUrl);
+  }
+
+  identify(body: EventBodyIdentify): void {
+    this.sendEvent("identify", body);
+  }
+
+  track(body: EventBodyTrack): void {
+    this.sendEvent("track", body);
+  }
+}
