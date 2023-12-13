6b5138368b4067e8e570b1760c1d8fbb8afeafdd
Ben Papillon <benpapillon@gmail.com>
2023-12-13 10:25:45 -0500

    Cleanup before going public

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import fetch from "node-fetch";
 
 import * as api from "./api/index";
-import { EventCapture } from "./events";
 
 const fetchApi: api.FetchAPI = fetch as unknown as api.FetchAPI;
 
 export interface Schematic {
   Accounts: api.AccountsApi;
   Companies: api.CompaniesApi;
-  EventCapture: EventCapture;
   Events: api.EventsApi;
   Features: api.FeaturesApi;
   Plans: api.PlansApi;
@@ -37,7 +35,6 @@ export function init(apiKey: string, opts?: SchematicOptions): Schematic {
   return {
     Accounts: new api.AccountsApi(config),
     Companies: new api.CompaniesApi(config),
-    EventCapture: new EventCapture(config),
     Events: new api.EventsApi(config),
     Features: new api.FeaturesApi(config),
     Plans: new api.PlansApi(config),
